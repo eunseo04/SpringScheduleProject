1f200fa6373a678cecc5270dd2a385bb0c1d8a54
eunseo04 <jes040616@naver.com>
2025-08-04 15:39:35 +0900

    Comment Function

--- src/main/java/org/example/scheduleproject/comment/CommentController.java
+++ src/main/java/org/example/scheduleproject/comment/CommentController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("posts/comments")
+@RequestMapping("/comments")
 public class CommentController {
     private final CommentService commentService;
 
-    @PutMapping("/{id}")
+    @PostMapping("/{id}")
     public CommentResponseDto addComment(@PathVariable Long id, @RequestBody CommentRequestDto dto) {
         return commentService.comment(id, dto);
     }
