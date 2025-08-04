530a6f9d656fe784b3e91cc6175bf37ddc3c0ae1
eunseo04 <jes040616@naver.com>
2025-08-04 15:13:09 +0900

    Comment Function

--- src/main/java/org/example/scheduleproject/comment/CommentRepository.java
+++ src/main/java/org/example/scheduleproject/comment/CommentRepository.java
@@ -1,11 +1,8 @@
 package org.example.scheduleproject.comment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
-@Repository
 public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
     List<CommentEntity> findByScheduleId(Long postId); // 특정 게시글의 댓글 조회
 }
